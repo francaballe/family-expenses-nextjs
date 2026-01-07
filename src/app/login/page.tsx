683ed536eb2e1c84e8a3622725b5bc9b3befaa07
Francisco Javier Caballe <francaballe@gmail.com>
2026-01-06 22:36:50 -0300

    cambiada version menor por correccion de bug.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -129,7 +129,7 @@ export default function LoginPage() {
 
             {/* Version */}
             <div className="mt-8 text-gray-500 text-sm">
-                Version: 3.1.0
+                Version: 3.1.1
             </div>
         </div>
     );
